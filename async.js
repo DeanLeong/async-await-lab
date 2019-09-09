332e4ba0de4a8225aab0aa2af9b1fd71cc2915dd
Michael Stromer <stroms38@gmail.com>
2019-09-09 16:58:18 -0400

    solution shows all recipes

--- async.js
+++ async.js
@@ -1,50 +1,68 @@
 // Async Await Lab
 // (API Docs)[https://recipix.tech/api/docs]
-
 // PART 1: Read Recipe Name
-findRecipes('banana');
+
+findRecipes('banana+nut+bread');
 async function findRecipes(recipeName) {
   // read recipe
-  let response = await axios.get(
-    `https://recipix.tech/api/search/?q=${recipeName}`
-  );
+  const node = document.getElementById( 'recipes' );
+  while (node.lastChild) {
+    node.removeChild(node.lastChild);
+  };
+  try {
+    let response = await axios.get(
+      `https://recipix.tech/api/search/?q=${recipeName}`
+    );
 
-  // document.getElementById('recipe-title').innerHTML = ???
-  // document.getElementById('recipe-url').href = ???
-
-  // showIngredients( ??? );
+    // PART 4 - Display multiple recipes
+    // Added to previous functions
+    const allRecipes = response.data.results;
+    allRecipes.forEach(recipe => {
+      showIngredients(recipe);
+    })
+  } catch(err) {
+    console.log(err);
+  }
 }
 
 // PART 2: Read Recipe Ingredients
 function showIngredients(recipe) {
+  // create an HTML card string
+  let card = `<div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title" id="recipe-title">${recipe.title}</h5>
+            <ul class="card-text" id="recipe-ingredients-${recipe.id}"></ul>
+            <a href="${recipe.url}" id="recipe-url" class="btn btn-primary">Learn more</a>
+          </div>
+        </div>`;
+  // inserts card string as HTML node.
+  document.getElementById('recipes').insertAdjacentHTML( 'beforeend', card );
+  
+  // PART 3: Display Recipe from Ingredients
+
+  // For each ingredient displayed, add a `click` event listener that
+  // will search for new recipes, using the selected ingredient.
+
   // show the ingredients
-  // iterare over the response data
-  // insert each ingredient into the DOM.
-  // choose whichever type of iterator for each ingredient..
   let ingredients = recipe.ingredients;
-  document.getElementById('recipe-ingredients').innerHTML = '';
   ingredients.forEach(function(ingredient) {
     let listItem = document.createElement('li');
-    // listItem.innerHTML = ???
-    // listItem.addEventListener('???', ???);
-    document.getElementById('recipe-ingredients').append(listItem);
+    listItem.innerHTML = ingredient;
+    listItem.addEventListener('click', function(event) {
+      findRecipes(ingredient);
+    });
+    document.getElementById(`recipe-ingredients-${recipe.id}`).append(listItem);
   });
-  // return ingredients;
 }
 
-// PART 3: Display Recipe from Ingredients
-
-// For each ingredient displayed, add a `click` event listener that
-// will search for new recipes, using the selected ingredient.
-// HINT: Check out line 29!
-
-// PART 4: Display Multiple Recipes
-
-// Each time we request recipes, we get an array inside the `results` array.
-// How would you display all ten `results` at once?
-// Use the existing functions `findRecipes` and `showIngredients` to display all ten recipe results.
-
 // PART 5 - Display Search bar results
+
+let searchBar = document.getElementById("recipe-form");
+searchBar.addEventListener('submit', function(event) {
+  event.preventDefault();
+  var text = document.getElementById("recipe-search").value;
+  findRecipes(text);
+})
 
 // Try to display recipes using a search bar for text input.
 // HINT: Start by uncommenting the search bar in index.html
