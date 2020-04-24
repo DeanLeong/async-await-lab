ae71804c5c9bb03482070e1edee693daf1ddac58
Steven VanWoerkom <steven.vanwoerkom@generalassemb.ly>
2020-04-24 17:20:47 -0400

    ported over changes

--- async.js
+++ async.js
@@ -1,68 +1,71 @@
-// Async Await Lab
-// (API Docs)[https://recipix.app/api/docs]
-// PART 1: Read Recipe Name
+// Async/Await w/ Try/Catch
+// API docs https://restcountries.eu/#api-endpoints-full-name
 
-findRecipes('banana+nut+bread');
-async function findRecipes(recipeName) {
-  // read recipe
-  const node = document.getElementById( 'recipes' );
-  while (node.lastChild) {
-    node.removeChild(node.lastChild);
-  };
-  try {
-    let response = await axios.get(
-      `https://recipix.app/api/search/?q=${recipeName}`
-    );
+// Step 1: Fetch Country data
 
-    // PART 4 - Display multiple recipes
-    // Added to previous functions
-    const allRecipes = response.data.results;
-    allRecipes.forEach(recipe => {
-      showIngredients(recipe);
-    })
-  } catch(err) {
-    console.log(err);
-  }
-}
+// function fetchData(country) {
+//   const url = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`
+//   axios.get(url)
+//     .then(res => {
+//       let data = res.data[0]
+//       const appendElement = document.querySelector('#country-data')
+//       while (appendElement.lastChild) {
+//         appendElement.removeChild(appendElement.lastChild)
+//       }
+//       showCountryData(data)
+//   })  
+// }
 
-// PART 2: Read Recipe Ingredients
-function showIngredients(recipe) {
-  // create an HTML card string
-  let card = `<div class="card" style="width: 18rem;">
-          <div class="card-body">
-            <h5 class="card-title" id="recipe-title">${recipe.title}</h5>
-            <ul class="card-text" id="recipe-ingredients-${recipe.id}"></ul>
-            <a href="${recipe.url}" id="recipe-url" class="btn btn-primary">Learn more</a>
-          </div>
-        </div>`;
-  // inserts card string as HTML node.
-  document.getElementById('recipes').insertAdjacentHTML( 'beforeend', card );
-  
-  // PART 3: Display Recipe from Ingredients
+// Step 2: Rewrite using Try/Catch & Async/Await
 
-  // For each ingredient displayed, add a `click` event listener that
-  // will search for new recipes, using the selected ingredient.
+// async function fetchData(country) {
+//   const url = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`
+//   try {
+//     let response = await axios.get(url)
+//     let data = response.data[0]
+//     removeCountry()
+//     showCountryData(data)
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
 
-  // show the ingredients
-  let ingredients = recipe.ingredients;
-  ingredients.forEach(function(ingredient) {
-    let listItem = document.createElement('li');
-    listItem.innerHTML = ingredient;
-    listItem.addEventListener('click', function(event) {
-      findRecipes(ingredient);
-    });
-    document.getElementById(`recipe-ingredients-${recipe.id}`).append(listItem);
-  });
-}
+// Step 3: Create dynamic HTML
 
-// PART 5 - Display Search bar results
+// function showCountryData(data) {
+//   let countryInfo = `
+//   <img id="flag" src="${data.flag}" alt="flag" style="width: 400px; height: auto;">
+//   <h2 id="country-name">${data.name}</h2>
+//   <h5 id="capital">${data.capital}</h5>
+//   `
+// }
 
-let searchBar = document.getElementById("recipe-form");
-searchBar.addEventListener('submit', function(event) {
-  event.preventDefault();
-  var text = document.getElementById("recipe-search").value;
-  findRecipes(text);
-})
+// Step 4: Append Country Data
 
-// Try to display recipes using a search bar for text input.
-// HINT: Start by uncommenting the search bar in index.html
+// function showCountryData(data) {
+//   let countryInfo = `
+//   <img id="flag" src="${data.flag}" alt="flag" style="width: 400px; height: auto;">
+//   <h2 id="country-name">${data.name}</h2>
+//   <h5 id="capital">${data.capital}</h5>
+//   `
+//   document.querySelector('#country-data').insertAdjacentHTML('beforeend', countryInfo )
+// }
+
+// Step 5: Dynamically search country using HTML form with eventHandler
+
+// const submit = document.querySelector('#country-form')
+
+// submit.addEventListener('submit', (e) => {
+//   e.preventDefault()
+//   const inputValue = document.querySelector('#country-search').value
+//   fetchData(inputValue)
+// })
+
+// Step 6: Remove previous country data
+
+// function removeCountry() {
+//   const appendElement = document.querySelector('#country-data')
+//   while (appendElement.lastChild) {
+//     appendElement.removeChild(appendElement.lastChild)
+//   }
+// }
